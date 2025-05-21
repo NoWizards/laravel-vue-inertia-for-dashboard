ade307721cbb7cdde92dc1a31e2da077795b8346
NoWizards <nowizards@hotmail.es>
2025-05-21 11:13:59 -0500

    add more composables

--- resources/js/Composables/useMonthlyPayment.js
+++ resources/js/Composables/useMonthlyPayment.js
@@ -9,8 +9,12 @@ export const useMonthlyPayment = (total, interestRate, duration) => {
         return initialPrice*monthlyInterestRate*(Math.pow(1+monthlyInterestRate, numberOfPayments))/ (Math.pow(1 + monthlyInterestRate,numberOfPayments)-1);
     })
 
-    return {
-        monthlyPayment
-    }
+    const totalPaid = computed(() => {
+        return (isRef(duration) ? duration.value : duration) * 12 * monthlyPayment.value
+      })
+    
+    const totalInterest = computed(() => totalPaid.value - (isRef(total) ? total.value : total))
+    
+    return { monthlyPayment, totalPaid, totalInterest }
 
 }
